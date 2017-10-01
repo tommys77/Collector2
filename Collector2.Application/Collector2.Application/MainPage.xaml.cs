27d66237fc696cd33067e0efe6bb7cdc75aba5e5
tommys77 <stenberg.tommy@gmail.com>
2017-10-01 15:37:35 +0200

    DataServer published, begin application dev

---
+++ Collector2.Application/Collector2.Application/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Collector2.Application
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
--- Collector2.Model/Review.cs
+++ Collector2.Model/Review.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,14 @@ namespace Collector2.Model
         public string SourceType { get; set; }
         public string Score { get; set; }
         public int ScorePercentage { get; set; }
+        [ForeignKey("ScoreSystem")]
         public int ScoreSystemId { get; set; }
+        [ForeignKey("Software")]
+        public int ItemId { get; set; }
+
+        public virtual ScoreSystem ScoreSystem { get; set; }
+        public virtual Software Software { get; set; }
+
 
     }
 }
