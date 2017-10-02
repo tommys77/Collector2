ceb486fa61b61ffd8613478d9289d1c26b9d6b2a
tommys77 <stenberg.tommy@gmail.com>
2017-10-02 22:44:21 +0200

    Need to rotate photo

--- Collector2.Android/MainActivity.cs
+++ Collector2.Android/MainActivity.cs
@@ -2,9 +2,17 @@
 using Android.Widget;
 using Android.OS;
 using System;
+using Java.IO;
+using Android.Graphics;
 using Android.Provider;
 using Android.Content;
 using Android.Runtime;
+using Uri = Android.Net.Uri;
+using Android.Support.V4.Content;
+using Environment = Android.OS.Environment;
+using Android.Util;
+using System.Collections.Generic;
+using Android.Content.PM;
 
 namespace Collector2.Android
 {
@@ -13,6 +21,12 @@ namespace Collector2.Android
     {
         private Button mCameraBtn;
         private Button mGalleryBtn;
+        public static class App
+        {
+            public static File _file;
+            public static File _dir;
+            public static Bitmap bitmap;
+        }
 
         private const string AUTHORITY = "collector2.fileprovider";
 
@@ -21,6 +35,11 @@ namespace Collector2.Android
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+            }
 
             mCameraBtn = FindViewById<Button>(Resource.Id.main_btn_camera);
             mCameraBtn.Click += TakePicture;
@@ -33,7 +52,7 @@ namespace Collector2.Android
         {
             App._file = new File(App._dir, String.Format("collector2_item{0}.jpg", Guid.NewGuid()));
             Intent camera = new Intent(MediaStore.ActionImageCapture);
-            camera.PutExtra(MediaStore.ExtraOutput, FileProvider.GetUriForFile(context, AUTHORITY, App._file));
+            camera.PutExtra(MediaStore.ExtraOutput, FileProvider.GetUriForFile(this, AUTHORITY, App._file));
             try
             {
                 StartActivityForResult(camera, 0);
@@ -47,11 +66,17 @@ namespace Collector2.Android
         protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
-
             Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
-            Uri contentUri = FileProvider.GetUriForFile(context, AUTHORITY, App._file);
+            Uri contentUri = FileProvider.GetUriForFile(this, AUTHORITY, App._file);
             mediaScanIntent.SetData(contentUri);
-            context.SendBroadcast(mediaScanIntent);
+            SendBroadcast(mediaScanIntent);
+
+            if (resultCode == Result.Ok)
+            {
+                Intent pictureActivity = new Intent(this, typeof(PictureActivity));
+                pictureActivity.PutExtra("path", App._file.Path);
+                StartActivity(pictureActivity);
+            }
         }
 
         private void SelectFromGallery(object sender, EventArgs e)
@@ -59,6 +84,21 @@ namespace Collector2.Android
             throw new NotImplementedException();
         }
 
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities = this.PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = new File(
+                Environment.GetExternalStoragePublicDirectory(
+                    Environment.DirectoryPictures), "Collector2_Photos");
+            App._dir.Mkdirs();
+        }
     }
 }
 
--- Collector2.Android/PictureActivity.cs
+++ Collector2.Android/PictureActivity.cs
@@ -8,19 +8,30 @@ using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Java.IO;
 using Android.Widget;
+using Android.Graphics;
 
 namespace Collector2.Android
 {
+    [Activity(Label = "Collector2.Android")]
     public class PictureActivity : Activity
     {
+        private ImageView mPicture;
+        private Bitmap mBitmap;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Picture);
-
+            mPicture = FindViewById<ImageView>(Resource.Id.picture_iv_picture);
+            var path = Intent.GetStringExtra("path");
+            //Toast.MakeText(this, path, ToastLength.Long).Show();
+            
+            var height = Resources.DisplayMetrics.HeightPixels;
+            var width = mPicture.Height;
+            mBitmap = path.LoadAndResizeBitmap(width, height);
+            mPicture.SetImageBitmap(mBitmap);
         }
     }
 }
