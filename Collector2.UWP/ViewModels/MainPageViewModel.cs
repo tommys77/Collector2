6f6c9068d787ffc4af2dabe1cb52647f0dfbcfff
tommys77 <stenberg.tommy@gmail.com>
2017-10-08 17:31:17 +0200

    Started designing main page

---
+++ Collector2.UWP/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using GalaSoft.MvvmLight;
+using Windows.UI.Xaml;
+
+namespace Collector2.UWP.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        private bool isPaneOpen;
+
+        public bool IsPaneOpen
+        {
+            get {
+                return isPaneOpen;
+            }
+            set {
+                if(!Equals(IsPaneOpen, value))
+                {
+                    isPaneOpen = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public void OpenPane()
+        {
+            IsPaneOpen = !IsPaneOpen;
+        }
+    }
+}
