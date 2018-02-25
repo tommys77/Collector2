3c956e6baa6ac41a870e133ebb6de29b445df186
tommys77 <stenberg.tommy@gmail.com>
2018-02-25 22:05:42 +0100

    Some Details

--- Collector2.UWP/ViewModels/SoftwareDetailsViewModel.cs
+++ Collector2.UWP/ViewModels/SoftwareDetailsViewModel.cs
@@ -1,15 +1,45 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Collector2.Models;
+using GalaSoft.MvvmLight;
 
 namespace Collector2.UWP.ViewModels
 {
     public class SoftwareDetailsViewModel : ViewModelBase
     {
+
+        private ItemImage _selectedItemImage;
+        private Software _selectedSoftware;
+
         public SoftwareDetailsViewModel()
         {
-            if(IsInDesignMode)
+            if (IsInDesignMode)
             {
 
             }
         }
+
+        public ItemImage SelectedItemImage {
+            get
+            {
+                return _selectedItemImage;
+            }
+            set
+            {
+                _selectedItemImage = value;
+                RaisePropertyChanged(nameof(SelectedItemImage));
+            }
+        }
+
+        private Software SelectedSoftware {
+            get
+            {
+                return _selectedSoftware;
+            }
+            set
+            {
+                _selectedSoftware = value;
+                RaisePropertyChanged(nameof(SelectedSoftware));
+            }
+        }
+
     }
 }
